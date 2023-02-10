4b1b4a2ecb60fc9ce450721e5e607795b3dc8008
MANI NN <manikandakaviyan@gmail.com>
2023-02-10 12:01:23 +0530

    Add project files.

---
+++ TimeeZone/Controllers/TimeZoneController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+using NodaTime.Extensions;
+using NodaTime.TimeZones;
+using TimeeZone.Model;
+
+namespace TimeeZone.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    public class TimeZoneController : Controller
+    {
+        [HttpGet("{city}")]
+        public ActionResult<TimeZoneModel> Get(string city)
+        {
+            try
+            {
+                var decodedId = System.Uri.UnescapeDataString(city);
+                var timeZone = DateTimeZoneProviders.Tzdb[decodedId];
+                var currentTime = SystemClock.Instance.GetCurrentInstant().InZone(timeZone).ToDateTimeOffset();
+
+                return new TimeZoneModel
+                {
+                    TimeZoneId = decodedId,
+                    CurrentTime = currentTime
+                };
+            }
+            catch (DateTimeZoneNotFoundException)
+            {
+                return BadRequest($"The time zone '{city}' was not found in the TZDB database.");
+            }
+        }
+    }
+
+
+
+
+}
---
+++ TimeeZone/Model/TimeZoneModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeeZone.Model
+{
+    public class TimeZoneModel
+    {
+        public string TimeZoneId { get; set; }
+        public DateTimeOffset CurrentTime { get; set; }
+    }
+}
